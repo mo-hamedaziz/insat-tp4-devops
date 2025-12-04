9f9be0910b222379f655281639a0594cd8f6fb07
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2025-12-04 23:05:47 +0100

    Add terraform variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,26 @@
+# --- Variables de Base de Données (PostgreSQL) ---
+variable "db_name" {
+  description = "Nom de la base de données PostgreSQL."
+  type        = string
+  default     = "devops_db"
+}
+
+variable "db_user" {
+  description = "Nom d'utilisateur PostgreSQL."
+  type        = string
+  default     = "devops_user"
+}
+
+variable "db_password" {
+  description = "Mot de passe PostgreSQL (ATTENTION: Simulé, ne pas utiliser en Prod !)"
+  type        = string
+  default     = "strongpassword123"
+  sensitive   = true  # Marque le mot de passe comme sensible
+}
+
 # --- Variables d'Application ---
 variable "app_port_external" {
-  description = "Port externe pour accéder à l'application web (mappé au port 80 interne)."
+  description = "Port externe pour accéder à l'application web (mappé au 80 interne)."
   type        = number
   default     = 8080
 }
